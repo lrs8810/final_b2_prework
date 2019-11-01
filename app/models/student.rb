73695cf6eb6c067a14a2b204af2f6300ed092e45
Laura Schulz <lrs8810@gmail.com>
2019-11-01 07:36:03 -0600

    Remove course_name_grade method

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,8 +3,4 @@ class Student < ApplicationRecord
   has_many :courses, through: :course_students
 
   validates_presence_of :name
-
-  def course_name_grade
-    courses.joins(:course_students).select('course_students.grade, courses.*')
-  end
 end
