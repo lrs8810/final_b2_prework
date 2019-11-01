bd2bd91f5a88c5a725ce7f176e3eb9f83b9b3a0a
Laura Schulz <lrs8810@gmail.com>
2019-11-01 04:44:30 -0600

    Add student show page spec

---
+++ spec/features/students/show_spec.rb
@@ -0,0 +1,47 @@
+# User Story 1, Student Show Page
+# As a user,
+# When I visit a student show page
+# Then I see
+# - the student's name
+# - the name of each course the student is enrolled in
+# - the student's grade for each course
+
+
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  describe 'when I visit a student show page' do
+    it 'I see the student\'s name, all their courses, and their grade per course' do
+      laura = Student.create(name: 'Laura')
+
+      biology = Course.create(name: 'Biology')
+      math = Course.create(name: 'Math')
+      history = Course.create(name: 'History')
+
+      biology_grade = biology.course_students.create(grade: 89.5)
+      math_grade = math.course_students.create(grade: 80.0)
+      history_grade = history.course_students.create(grade: 94.0)
+
+      laura.course_students.push(biology_grade, math_grade, history_grade)
+
+      visit "/students/#{laura.id}"
+
+      expect(page).to have_content('Laura')
+
+      within "#course-#{biology.id}" do
+        expect(page).to have_content('Biology')
+        expect(page).to have_content('Grade: 89.5')
+      end
+
+      within "#course-#{math.id}" do
+        expect(page).to have_content('Math')
+        expect(page).to have_content('Grade: 80.0')
+      end
+
+      within "#course-#{history.id}" do
+        expect(page).to have_content('History')
+        expect(page).to have_content('Grade: 94.0')
+      end
+    end
+  end
+end
