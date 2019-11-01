0e84a626b4bb038608ea2c41165274b7df5da9d8
Laura Schulz <lrs8810@gmail.com>
2019-11-01 04:44:45 -0600

    Add skeleton of course show page spec

---
+++ spec/features/courses/show_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  describe 'when I visit a course show page' do
+    it 'I see the course name and all students in the course ordered from highest to lowest grade' do
+    end
+  end
+end
