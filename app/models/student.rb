c07f8dce18dc244b7996c032ce4de2d468003b8b
Laura Schulz <lrs8810@gmail.com>
2019-11-01 05:40:03 -0600

    Add instance method to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,8 @@ class Student < ApplicationRecord
   has_many :courses, through: :course_students
 
   validates_presence_of :name
+
+  def course_name_grade
+    Course.joins(:course_students).select('course_students.grade, courses.*')
+  end
 end
